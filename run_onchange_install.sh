27a290faa5f39f74cd4cb3d65b350cb5857d4e1d
Aymerick Dupouey <a.dupouey@lectra.com>
2024-06-13 14:23:18 +0200

    fix: named home dir

--- run_onchange_install.sh
+++ run_onchange_install.sh
@@ -2,11 +2,11 @@
 
 curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
 
-wget -qO- $(curl -sL https://api.github.com/repos/junegunn/fzf/releases/latest | jq -r '.assets[] | select(.name | test("^fzf-.*-linux_amd64.tar.gz$")) | .browser_download_url') | tar -zxvf - fzf && install -c -m 0755 fzf /home/dupouey/.local/bin/fzf
+wget -qO- $(curl -sL https://api.github.com/repos/junegunn/fzf/releases/latest | jq -r '.assets[] | select(.name | test("^fzf-.*-linux_amd64.tar.gz$")) | .browser_download_url') | tar -zxvf - fzf && install -c -m 0755 fzf ~/.local/bin/fzf
 
 az extension add --name fzf
 
-wget -qO- $(curl -sL https://api.github.com/repos/kubecolor/kubecolor/releases/latest | jq -r '.assets[] | select(.name | test("^kubecolor_.*_linux_amd64.tar.gz$")) | .browser_download_url') | tar -zxvf - kubecolor && install -c -m 0755 kubecolor /home/dupouey/.local/bin/_kubecolor
+wget -qO- $(curl -sL https://api.github.com/repos/kubecolor/kubecolor/releases/latest | jq -r '.assets[] | select(.name | test("^kubecolor_.*_linux_amd64.tar.gz$")) | .browser_download_url') | tar -zxvf - kubecolor && install -c -m 0755 kubecolor ~/.local/bin/_kubecolor
 
 wget -O ~/.kubectl_aliases https://raw.githubusercontent.com/ahmetb/kubectl-aliases/master/.kubectl_aliases
 sed -ri 's/owide//g' ~/.kubectl_aliases
